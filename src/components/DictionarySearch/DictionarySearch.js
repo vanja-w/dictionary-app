ec21e2e67c35ad19dd8f6d9e2254581d1a5ca1ae
vanja-w <vanja.wallace@gmail.com>
2022-05-15 17:27:09 +0200

    API integration

--- src/components/DictionarySearch/DictionarySearch.js
+++ src/components/DictionarySearch/DictionarySearch.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
+import axios from "axios";
+import DictionaryResults from "../DictionaryResults/DictionaryResults";
+
+let countSubmit = 0;
+let countGetResponse = 0;
 
 export default function DictionarySearch() {
   // create a state
-  const [word, setWord] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [apiResults, setApiResults] = useState(null);
 
   // handle user input
   function handleSearchWord(event) {
-    setWord(event.target.value);
+    setUserInput(event.target.value);
   }
 
   //handle submit
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for "${word}"..`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`;
+    axios.get(apiUrl).then(getResponse);
+    countSubmit++;
+    console.log("countSubmit " + countSubmit);
+  }
+
+  //get API response and store it in state apiResults
+  function getResponse(response) {
+    setApiResults(response.data[0]);
+    countGetResponse++;
+    console.log("countGetResponse " + countGetResponse);
   }
 
   return (
@@ -23,11 +40,13 @@ export default function DictionarySearch() {
           onChange={handleSearchWord}
           type="search"
           className="form-control-lg"
-          placeholder="Search for a word"
+          placeholder="Search for a userInput"
         />
         <input type="submit" className="btn btn-primary btn-lg" />
       </form>
       {/*FORM ENDS*/}
+
+      <DictionaryResults apiResults={apiResults} />
     </div>
   );
 }
