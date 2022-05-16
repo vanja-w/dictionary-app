e67236da7dcd531b5631b2c9f9d8a29a24e1ee0b
vanja-w <vanja.wallace@gmail.com>
2022-05-16 16:11:59 +0200

    Adds rel="noreferrer"

--- src/components/Phonetics/Phonetics.js
+++ src/components/Phonetics/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
 
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Audio
       </a>
       {props.phonetic.text}
