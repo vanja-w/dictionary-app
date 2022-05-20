99064c139a11ddfbcc827fd6b1b5b8732cdc7aab
vanja-w <vanja.wallace@gmail.com>
2022-05-20 16:14:16 +0200

    Installs react-audio-player package
    react-audio-player allows users to play the audio of the given phonetic(s) while on the page. Before, the users would click on a link that would redirect them to a new page where they could play the audio of the phonetics.
    
    documentation:
    
    https://github.com/justinmc/react-audio-player#readme

--- src/components/Phonetics/Phonetics.js
+++ src/components/Phonetics/Phonetics.js
@@ -1,14 +1,14 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   console.log(props.phonetic);
 
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Audio
-      </a>
-      {props.phonetic.text}
+      <ReactAudioPlayer src={props.phonetic.audio} controls />
+      <p> {props.phonetic.text}</p>
     </div>
   );
 }
